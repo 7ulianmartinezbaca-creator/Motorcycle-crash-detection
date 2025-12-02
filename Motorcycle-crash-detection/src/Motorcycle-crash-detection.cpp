f26b8df413958e92148ad4258f00489cc21cbdf9
Julian Martinez Baca <j@Julians-MacBook-Air.local>
2025-12-02 14:49:33 -0700

    work in progress

--- Motorcycle-crash-detection/src/Motorcycle-crash-detection.cpp
+++ Motorcycle-crash-detection/src/Motorcycle-crash-detection.cpp
@@ -8,45 +8,31 @@
 
 // Include Particle Device OS APIs
 #include "Particle.h"
-
+#include "HC_SR04.h"
 
 SYSTEM_MODE(SEMI_AUTOMATIC);
 
-long microsecondsToInches(long microseconds);
+double cm = 0.0;
+double inches = 0.0;
 
-const int trigpin = D14;
-const int echopin = D19;
+int trigpin = D14;
+int echopin = D19;
 
-int duration, inches;
+HC_SR04 rangefinder = HC_SR04(trigpin, echopin);
 
 void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected,10000);
 
-  pinMode(trigpin,INPUT);
-  pinMode(echopin,OUTPUT);
+  Spark.variable("cm", &cm, DOUBLE);
+  Spark.variable("inches", &inches, DOUBLE);
 }
 
 // loop() runs over and over again, as quickly as it can execute.
 void loop() {
-  digitalWrite(echopin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(echopin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(echopin, LOW);
-
-  duration = pulseIn(trigpin, HIGH);
-
-  inches = microsecondsToInches(duration);
+  cm = rangefinder.getDistanceCM();
+  inches = rangefinder.getDistanceInch();  
 
   Serial.printf("%i\n",inches);
 }
 
-long microsecondsToInches(long microseconds) {
-  // According to Parallax's datasheet for the PING))), there are 73.746
-  // microseconds per inch (i.e. sound travels at 1130 feet per second).
-  // This gives the distance travelled by the ping, outbound and return,
-  // so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
-}
